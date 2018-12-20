de9abb891424725f3eb407a2816fddbc2e20252a
wenzhi <wenzhi@uoko.com>
2018-12-20 16:51:19 +0800

    Loading

---
+++ app/src/main/java/com/mob/lee/fastair/view/LoadView.kt
@@ -0,0 +1,99 @@
+package com.mob.lee.fastair.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.mob.lee.fastair.R
+import kotlin.math.*
+
+class LoadView : View {
+
+    var mCircleCount=13
+    var mLastIndex=0
+    var minSize=48F
+
+    val mPaint: Paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            this.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        }
+    }
+    val mCircles=Array(mCircleCount,{
+        Circle(0F,0F)
+    })
+
+    constructor(context: Context) : super(context, null)
+
+    constructor(context: Context, attri: AttributeSet?) : super(context, attri)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var width=MeasureSpec.getSize(widthMeasureSpec)
+        var height=MeasureSpec.getSize(heightMeasureSpec)
+        val size=TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,minSize,context.resources.displayMetrics).toInt()
+        if(MeasureSpec.UNSPECIFIED!=MeasureSpec.getMode(widthMeasureSpec)){
+            width=size
+        }
+        if(MeasureSpec.UNSPECIFIED!=MeasureSpec.getMode(heightMeasureSpec)){
+            height=size
+        }
+        setMeasuredDimension(width,height)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        val centerX = width / 2F
+        val centerY = height / 2F
+        val contentWidth = min(centerX - paddingLeft, centerY - paddingTop)
+        val smallRadiu = contentWidth / 10F
+        val bigRadiu = contentWidth - smallRadiu
+        for(c in mCircles){
+            c.radius=smallRadiu
+            c.bigRadius=bigRadiu
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        val centerX = width / 2F
+        val centerY = height / 2F
+        if(mLastIndex<mCircleCount-1){
+            val circle=mCircles[mLastIndex]
+            val angle= sin(circle.radius/2/circle.bigRadius)*180/ PI
+
+            if(circle.angle+90> 5*angle){
+                mLastIndex+=1
+            }
+        }
+        for(i in 0 .. mLastIndex){
+            val c=mCircles[i]
+            c.update()
+            val x = centerX + c.x
+            val y = centerY + c.y
+            canvas?.drawCircle(x, y, c.radius, mPaint)
+        }
+        invalidate()
+    }
+
+    data class Circle(var radius:Float,var bigRadius: Float) {
+        var x: Float = 0F
+        var y: Float = 0F
+        var angle: Float = -90F
+        var acceleration=3.14F
+
+        fun update() {
+            if (angle >= 270) {
+                angle = -90F
+            }else {
+                angle += acceleration
+            }
+            val rate=(angle+90)/360
+            val output = (cos((rate + 1) * Math.PI.toFloat()) / 2.0F) + 0.5F
+            val radian = (-90+360*output) * (PI / 180).toFloat()
+            x = bigRadius * cos(radian)
+            y = bigRadius * sin(radian)
+        }
+    }
+}
--- app/src/main/java/com/mob/lee/fastair/view/LoadingView.kt
+++ app/src/main/java/com/mob/lee/fastair/view/LoadingView.kt
@@ -1,17 +1,210 @@
 package com.mob.lee.fastair.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
 import android.view.View
+import androidx.annotation.IntDef
+import androidx.core.content.ContextCompat
+import com.mob.lee.fastair.R
+import java.lang.annotation.RetentionPolicy
+import kotlin.math.min
+import kotlin.properties.Delegates
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-class LoadingView: View {
+class LoadingView : View {
 
-    constructor(context:Context):this(context,null)
+    companion object {
+        @JvmStatic
+        val DEFAULT_SIZE = 96F
 
-    constructor(context : Context,attri:AttributeSet?):super(context,attri)
+        val TAG = "LoadingView"
 
-    override fun onMeasure(widthMeasureSpec : Int, heightMeasureSpec : Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        const val RAIN_STATE_CREATE = 0
+
+        const val RAIN_STATE_DROP = 1
+
+        const val RAIN_STATE_REACH = 2
+
+        const val RAIN_STATE_DIFFUSION = 3
+
+        const val RAIN_STATE_END = 4
+    }
+
+    val mPaint: Paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            this.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        }
+    }
+
+    val mRandom = Random(System.currentTimeMillis())
+
+    val mRain = Array<Rain>(256, {
+        genOrUpdate()
+    })
+
+    var mCuurentActive = 1
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attri: AttributeSet?) : super(context, attri)
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        for (r in mRain) {
+            genOrUpdate(r)
+        }
+        setLayerType(LAYER_TYPE_SOFTWARE, mPaint)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas ?: return
+
+        drawRain(canvas)
+    }
+
+    fun drawRain(canvas: Canvas) {
+        changeActive()
+        for (r in mRain) {
+            if (r.state == RAIN_STATE_CREATE) {
+                continue
+            }
+            r.update(width, height)
+            canvas.save()
+            canvas.translate(r.x, r.y)
+            mPaint.alpha = r.alpha
+            canvas.drawPath(r.path, mPaint)
+            canvas.restore()
+
+        }
+        postDelayed({ invalidate() }, 32)
+    }
+
+    fun genOrUpdate(raim: Rain? = null): Rain {
+        val velocity = mRandom.nextFloat() * 50
+        val quality = mRandom.nextFloat() * 20
+        val x = mRandom.nextInt(if (0 == width) 2 else width).toFloat()
+        val y = (mRandom.nextInt(-100, -20)).toFloat()
+        return raim?.apply {
+            this.velocity = velocity
+            this.quility = quality
+            this.x = x
+            this.y = y
+        } ?: Rain(velocity, quality, x, y).apply { state = RAIN_STATE_CREATE }
+    }
+
+    fun changeActive() {
+        mCuurentActive += 1
+        if (mCuurentActive > mRain.size) {
+            mCuurentActive = mRain.size
+        }
+        var count = 0
+        for (rain in mRain) {
+            when{
+                rain.state==RAIN_STATE_CREATE&&count < mCuurentActive -> {
+                        rain.state = RAIN_STATE_DROP
+                        count += 1
+                }
+                rain.state==RAIN_STATE_END&&count < mCuurentActive -> {
+                    genOrUpdate(rain)
+                    rain.state= RAIN_STATE_CREATE
+                    rain.update(width, height)
+                }
+
+                rain.state>RAIN_STATE_CREATE -> rain.update(width, height)
+            }
+        }
 
     }
+
+    data class Rain(
+            var velocity: Float,
+            var quility: Float,
+            var x: Float,
+            var y: Float,
+            val width: Int = 16,
+            val height: Int = 24
+    ) {
+        var alpha = 255
+        var diffuseVel = 1F
+        val path: Path
+        lateinit var bound: RectF
+        @RainState
+        var state: Int = RAIN_STATE_CREATE
+
+        init {
+            path = Path()
+            genRain()
+        }
+
+        fun update(totalWidth: Int,
+                   totalHeight: Int) {
+            when (state) {
+                RAIN_STATE_CREATE -> {
+                    state = RAIN_STATE_DROP
+                    path.reset()
+                    genRain()
+                }
+
+                RAIN_STATE_DROP -> {
+                    alpha = 255
+                    velocity += 0.013F * velocity
+                    y += velocity
+                    if (y + height >= 9 * totalHeight / 10) {
+                        state = RAIN_STATE_REACH
+                    }
+                }
+
+                RAIN_STATE_REACH -> {
+                    state = RAIN_STATE_DIFFUSION
+                    path.reset()
+                    diffuseVel = quility / 10
+                    bound = RectF(0F, 9 * height / 10F, width.toFloat(), height.toFloat())
+                    path.addOval(bound, Path.Direction.CW)
+                }
+
+                RAIN_STATE_DIFFUSION -> {
+                    diffuseVel += 0.098F * diffuseVel
+                    alpha -= 20
+                    bound.left -= diffuseVel
+                    bound.top -= diffuseVel
+                    bound.right += diffuseVel
+                    bound.bottom += diffuseVel
+                    path.reset()
+                    path.addOval(bound, Path.Direction.CW)
+                    if (0 >= alpha) {
+                        alpha = 0
+                        state = RAIN_STATE_END
+                    }
+                }
+
+                RAIN_STATE_END -> {
+                    path.reset()
+                    genRain()
+                }
+            }
+        }
+
+        private fun genRain() {
+            path.moveTo(width / 2F, height.toFloat())
+            path.quadTo(width / 18F, 11 * height / 12F, 3 * width / 10F, 3 * height / 7F)
+            path.quadTo(9 * width / 20F, 3 * height / 14F, width / 2F, 0F)
+            path.quadTo(11 * width / 20F, 3 * height / 14F, 7 * width / 10F, 3 * height / 7F)
+            path.quadTo(17 * width / 18F, 11 * height / 12F, width / 2F, height.toFloat())
+        }
+    }
+
+    @IntDef(RAIN_STATE_CREATE, RAIN_STATE_DROP, RAIN_STATE_REACH, RAIN_STATE_DIFFUSION, RAIN_STATE_END)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class RainState {}
 }
+
+
