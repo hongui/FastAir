bdb9b512a3a576fcaf837db99d7be8c99e1ece68
andy <honguilee@163.com>
2018-12-24 17:14:21 +0800

    数据更新

--- app/src/main/java/com/mob/lee/fastair/fragment/ContentPickFragment.kt
+++ app/src/main/java/com/mob/lee/fastair/fragment/ContentPickFragment.kt
@@ -1,16 +1,21 @@
 package com.mob.lee.fastair.fragment
 
+import android.graphics.Color
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.CheckBox
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mob.lee.fastair.R
 import com.mob.lee.fastair.adapter.Adapter
-import com.mob.lee.fastair.adapter.ContentPickAdapter
+import com.mob.lee.fastair.adapter.MultiDataHolder
 import com.mob.lee.fastair.adapter.SimgleDataHolder
 import com.mob.lee.fastair.base.AppFragment
-import com.mob.lee.fastair.model.Record
+import com.mob.lee.fastair.model.*
+import com.mob.lee.fastair.utils.display
 import com.mob.lee.fastair.utils.updateStorage
 import com.mob.lee.fastair.viewmodel.FileViewModel
 import kotlinx.android.synthetic.main.fragment_content_pick.*
@@ -20,15 +25,43 @@ import java.io.File
  * Created by Andy on 2017/6/20.
  */
 class ContentPickFragment : AppFragment() {
-    override fun layout() : Int = R.layout.fragment_content_pick
+    override fun layout(): Int = R.layout.fragment_content_pick
 
     override fun setting() {
-        val adapter = Adapter(ContentPickAdapter())
+        val viewModel = ViewModelProviders.of(mParent!!).get(FileViewModel::class.java)
+
+        val contentDataHolder = MultiDataHolder<Record>(R.layout.item_file, { position, record, viewHolder ->
+            record ?: return@MultiDataHolder
+            viewHolder.text(R.id.item_file_name, record.name)
+            viewHolder.text(R.id.item_file_extra, record.date.formatDate() + "\t\t" + record.size.formatSize(viewHolder.itemView.context))
+            val iconView = viewHolder.view<ImageView>(R.id.item_file_icon)
+            val checkBox = viewHolder.view<CheckBox>(R.id.item_file_selector)
+            if (null != iconView) {
+                display(viewHolder.itemView.context, record.path, iconView)
+            }
+
+            viewHolder.itemView.setOnClickListener {
+                val state = if (checkBox?.isChecked ?: false) {
+                    STATE_ORIGIN
+                } else {
+                    STATE_CHECK
+                }
+                viewModel.updateState(mScope, state, position, 1)
+            }
+            viewHolder.check(R.id.item_file_selector, STATE_CHECK == record.state)
+            val isChecked = viewHolder.view<CheckBox>(R.id.item_file_selector)?.isChecked ?: false
+            val color = if (isChecked) {
+                ContextCompat.getColor(viewHolder.itemView.context, R.color.material_grey_300)
+            } else {
+                Color.WHITE
+            }
+            viewHolder.itemView.setBackgroundColor(color)
+        })
+        val adapter = Adapter(contentDataHolder)
 
         pickContent.layoutManager = LinearLayoutManager(context)
         pickContent.adapter = adapter
 
-        val viewModel = ViewModelProviders.of(mParent!!).get(FileViewModel::class.java)
         viewModel.record.observe({ lifecycle }) {
             it?.let {
                 adapter.change(it)
@@ -53,13 +86,17 @@ class ContentPickFragment : AppFragment() {
                 }
             }
         }
+
+        viewModel.update.observe({ lifecycle }) {
+            adapter.change(it)
+        }
     }
 
-    override fun onPrepareOptionsMenu(menu : Menu?) {
+    override fun onPrepareOptionsMenu(menu: Menu?) {
         super.onPrepareOptionsMenu(menu)
         val allMenu = menu?.findItem(R.id.menu_content_all)
         val swapMenu = menu?.findItem(R.id.menu_content_swap)
-        val viewModel = ViewModelProviders.of(activity !!).get(FileViewModel::class.java)
+        val viewModel = ViewModelProviders.of(activity!!).get(FileViewModel::class.java)
         allMenu?.title = if (viewModel.mCheckAll) {
             getString(R.string.selectAll)
         } else {
@@ -72,12 +109,12 @@ class ContentPickFragment : AppFragment() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu : Menu?, inflater : MenuInflater?) {
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         inflater?.inflate(R.menu.menu_pick, menu)
     }
 
-    override fun onOptionsItemSelected(item : MenuItem?) : Boolean {
-        val viewModel = ViewModelProviders.of(activity !!).get(FileViewModel::class.java)
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val viewModel = ViewModelProviders.of(activity!!).get(FileViewModel::class.java)
 
         when (item?.itemId) {
             R.id.menu_content_delete -> {
@@ -92,7 +129,7 @@ class ContentPickFragment : AppFragment() {
             }
             R.id.menu_content_swap -> {
                 viewModel.reverse(mScope)
-                viewModel.mIsDes = ! viewModel.mIsDes
+                viewModel.mIsDes = !viewModel.mIsDes
                 if (viewModel.mIsDes) {
                     item.title = getString(R.string.des)
                 } else {
@@ -120,15 +157,15 @@ class ContentPickFragment : AppFragment() {
         return true
     }
 
-    fun deleteFiles(data : List<Record>) {
-        val adapter = pickContent.adapter as ContentPickAdapter
+    fun deleteFiles(data: List<Record>) {
+        val adapter = pickContent.adapter
         var count = 0
         data.dropLastWhile {
             val file = File(it.path)
             val success = file.delete()
             if (success) {
-                updateStorage(context !!, it)
-                count ++
+                updateStorage(context!!, it)
+                count++
                 //mDataHolder.delete(it)
             }
             success
