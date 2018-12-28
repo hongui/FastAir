7bfb384e57042f6d5884280a26b7bfab8a0eb53d
hongui <honguilee@163.com>
2018-12-28 22:51:45 +0800

    修改了一下连接问题，聊天功能依旧有问题

--- app/src/main/java/com/mob/lee/fastair/p2p/ActionListener.kt
+++ app/src/main/java/com/mob/lee/fastair/p2p/ActionListener.kt
@@ -10,40 +10,39 @@ import com.mob.lee.fastair.utils.errorToast
 /**
  * Created by Andy on 2017/8/17.
  */
-class ActionListener(val context: Context?) : WifiP2pManager.ActionListener {
+class ActionListener(val context : Context?) : WifiP2pManager.ActionListener {
     override fun onSuccess() {
 
     }
 
-    override fun onFailure(reason: Int) {
+    override fun onFailure(reason : Int) {
         when (reason) {
 
             WifiP2pManager.P2P_UNSUPPORTED -> {
-                val activity =context as? AppActivity
+                val activity = context as? AppActivity
                 activity?.let {
                     AlertDialog.Builder(it)
                             .setTitle(R.string.wramTips)
                             .setMessage(R.string.tip_error_p2p_unsupport)
-                            .setPositiveButton(R.string.knowIt,{dialog, which ->
+                            .setPositiveButton(R.string.knowIt, { dialog, which ->
                                 activity.supportFinishAfterTransition()
                             })
-                }?:let {
+                } ?: let {
                     context?.errorToast(R.string.tip_error_p2p_unsupport)
                 }
             }
 
-            WifiP2pManager.BUSY ->{
+            WifiP2pManager.BUSY -> {
                 context?.let {
                     it.errorToast(R.string.tip_error_p2p_busy)
-                    P2PManager.unregister(context)
-                    P2PManager.register(context.applicationContext)
+                    P2PManager.stopConnect(it)
                 }
             }
 
             WifiP2pManager.ERROR -> context?.let {
                 it.errorToast(R.string.tip_error_p2p_inter)
-                P2PManager.unregister(context)
-                P2PManager.register(context.applicationContext)
+                P2PManager.stopConnect(it)
+
             }
         }
     }
