dd1ec91fa08c533774dec9c8959001b79d4d60f9
lihonggui <li.honggui@navercorp.com>
2022-01-26 14:57:12 +0800

    update resource

---
+++ FastSvelte/src/lib/com/CategoryItem.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let current;
+	export let record;
+	import Image from '$lib/com/Image.svelte';
+
+	import MusicSVG from '$lib/icons/Music.svelte';
+	import VideoSVG from '$lib/icons/Video.svelte';
+	import WordSVG from '$lib/icons/Word.svelte';
+	import ExcelSVG from '$lib/icons/Excel.svelte';
+	import PptSVG from '$lib/icons/Ppt.svelte';
+	import TxtSVG from '$lib/icons/Txt.svelte';
+	import PdfSVG from '$lib/icons/Pdf.svelte';
+	import ApkSVG from '$lib/icons/Apk.svelte';
+	import ZipSVG from '$lib/icons/Zip.svelte';
+	import UnknowSVG from '$lib/icons/Unknow.svelte';
+
+	import Universal from '$lib/com/Universal.svelte';
+
+
+	let icons=[null,MusicSVG,VideoSVG,WordSVG,ExcelSVG,PptSVG,TxtSVG,PdfSVG,ApkSVG,ZipSVG,UnknowSVG]
+
+</script>
+
+{#if 0 === current}
+	<Image record={record}/>
+{:else}
+	<Universal Icon={icons[current]} record={record}/>
+{/if}
---
+++ FastSvelte/src/lib/com/Universal.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let Icon;
+    export let record;
+
+    let units=['B','KB','MB','GB']
+
+    let size=()=>{
+        let index=0;
+        let extra=record.size;
+        while(index<4&&Math.round(extra/1024)>0){
+            extra=extra/1024;
+            index++;
+        }
+        return extra.toFixed(2)+" " +units[index];
+    }
+
+</script>
+<a href="/download{record.path}">
+    <Icon style="width: 24px;
+    height: 24px;
+    margin: 12px;"/>
+    <h3 class="content">{record.name}</h3>
+    <span class="content">{size()}</span>
+</a>
+
+<style>
+    a{
+        display: flex;
+        width: 100%;
+        align-items: center;
+        color: var(--primary-color);
+    }
+
+    .content{
+        flex: auto;
+        margin-right: 16px;
+        color: var(--text-color);
+    }
+
+    h3{
+        flex: auto;
+    }
+
+</style>
